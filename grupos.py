ace6b1c12666b887e40a6b30392ee6f9763771cf
Melissa <melissapaccaossi@gmail.com>
2024-05-01 20:06:42 -0300

    grupos add

---
+++ grupos.py
@@ -0,0 +1,95 @@
+#importamos librerias
+import pandas as pd
+from math import sqrt #importaremos la funcion sqrt para la libreria math
+import numpy as np
+import matplotlib.pyplot as plt
+
+print('hola')
+
+peliculas = pd.read_csv('movies.csv')
+rating = pd.read_csv('rating.csv')
+grupos = pd.read_csv('grupos.csv')
+gruposcopia = pd.read_csv('gruposCopy.csv')
+
+
+print('Grupos: \n',grupos.head())
+print('Rating: \n',rating.head())
+
+gruposcopia['etiquetas'] = gruposcopia.etiquetas.str.split('|')
+
+grupos_co = gruposcopia.copy()
+for index, row in gruposcopia.iterrows():
+    for etiquetas in row['etiquetas']:
+        grupos_co.at[index,etiquetas] = 1
+
+grupos_co = grupos_co.fillna(0)
+
+print('Grupos codificadas: \n', grupos_co)
+
+usuario_en = [
+                {'nombre':'Salta Cultural Tour','rating':3},                
+                {'nombre':'Bariloche Hiking Adventure','rating':5},                
+                {'nombre':'Viedma Birdwatching','rating':4},                
+            ]
+
+entrada_grupos = pd.DataFrame(usuario_en)
+print('Grupos Usuario: \n', entrada_grupos)
+
+Id = gruposcopia[gruposcopia['nombre'].isin(entrada_grupos['nombre'].tolist())]
+entrada_grupos = pd.merge(Id, entrada_grupos)
+
+grupos_usuario = grupos_co[grupos_co['grupoID'].isin(entrada_grupos['grupoID'].tolist())]
+print('Grupos Usuario Codificados: \n', grupos_usuario)
+
+# grupos_usuario = grupos_usuario.reset_index(drop=True)
+# tabla_etiquetas = grupos_usuario.drop('grupoID', 1).drop('nombre', 1).drop('etiquetas',1)
+# print('Categorias que el usuario prefiere: \n', tabla_etiquetas)
+
+grupos_usuario = grupos_usuario.reset_index(drop=True)
+columnas_a_eliminar = ['grupoID', 'nombre', 'etiquetas']
+tabla_etiquetas = grupos_usuario.drop(columnas_a_eliminar, axis=1)
+print('Tabla Etiquetas: \n', tabla_etiquetas)
+
+
+perfil_usu = tabla_etiquetas.transpose().dot(entrada_grupos['rating'])
+print('Etiquetas que el usuario prefiere: \n', perfil_usu)
+
+# generos = peliculas_co.set_index(peliculas_co['ID'])
+
+# generos_a_mostrar = ['ID','Titulo','Genero']
+# generos = generos.drop(generos_a_mostrar, axis=1)
+# print('Generos: \n', generos.head())
+# generos.shape
+
+# recom = ((generos*perfil_usu).sum(axis=1))/(perfil_usu.sum())
+# print('Recomendaciones: \n', recom.head())
+
+# recom = recom.sort_values(ascending=False)
+# print('Recomendaciones Organizadas: \n', recom.head())
+
+# final = peliculas.loc[peliculas['ID'].isin(recom.head(5).keys())]
+# nfinal = final[['Titulo']]
+# print('Nombre de peliculas recomendadas: \n', nfinal)
+
+
+# # Realizar un join entre las recomendaciones y la tabla de películas
+# recomendaciones_organizadas = pd.DataFrame(recom, columns=['Similaridad'])
+# recomendaciones_organizadas.reset_index(inplace=True)
+# recomendaciones_organizadas.rename(columns={'index': 'ID'}, inplace=True)
+
+# recomendaciones_con_info = pd.merge(recomendaciones_organizadas, peliculas, on='ID', how='left')
+
+# print('Recomendaciones con información de películas: \n', recomendaciones_con_info.head())
+
+
+# # Lista de títulos de películas del usuario
+# titulos_usuario = [entrada['Titulo'] for entrada in usuario_en]
+
+# # Filtrar las recomendaciones para excluir las películas del usuario
+# recomendaciones_filtradas = recomendaciones_con_info[~recomendaciones_con_info['Titulo'].isin(titulos_usuario)]
+
+# print('Recomendaciones sin películas del usuario: \n', recomendaciones_filtradas.head())
+
+
+
+
